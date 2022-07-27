33a680e153a61426b4d610898aa02d3cfa996881
Joan Marcos <joanmrc96@gmail.com>
2022-07-27 10:58:08 -0300

    feat: adicionado correcao app

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import imp
-from login import login_exec
-from char_select import char_exec
+#from login import login_exec
+#from char_select import char_exec
+from auto_attack import auto_attack_by_images, JAULA_70
 
-login_exec()
+#login_exec()
 
-char_exec()
+#char_exec()
+
+auto_attack_by_images(JAULA_70)
